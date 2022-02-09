03f478a5527f6ab13b01166c328aff14215cc966
bneveret <eve20003@byui.edu>
2022-02-09 09:32:25 -0700

    small changes to word

--- Jumper_game.py/rand_word.py
+++ Jumper_game.py/rand_word.py
@@ -6,9 +6,9 @@ class Word:
     def __init__(self):
         '''creates a new word and gets length '''
 
-        self.words = ("python", "cheese", "jello", "easy", "hawks", "long", "common", "difficult")
+        self.words = ["python", "cheese", "jello", "easy", "hawks", "long", "common", "difficult"]
         self.word = random.choices(self.words)
-        self.len = self.word.len()
+        self.length = len(self.word)
 
     def get_letter(self):
         '''Gets input for a guessed letter from user'''
@@ -19,8 +19,8 @@ class Word:
     def guess_lines(self):
         '''creates lines to show length of word with letters filled out as game progresses'''
 
-        blanks = '_' * self.len
-        for i in range(self.len):
+        blanks = '_' * self.length
+        for i in range(self.length):
             if self.user_guess[i] in self.word:
                 blanks = blanks[:i] + self.word[i] + blanks[i+1:]
 
