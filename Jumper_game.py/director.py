681b59ce6afd9a201563bd9f29399151d4676d37
bneveret <eve20003@byui.edu>
2022-02-09 09:33:23 -0700

    mild editing and start on director

--- Jumper_game.py/director.py
+++ Jumper_game.py/director.py
@@ -3,3 +3,63 @@ from terminal_service import TerminalService
 from parachute_display import ParachuteMan
 
 class Director:
+    """A person who directs the game. 
+
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        hider (Hider): The game's hider.
+        is_playing (boolean): Whether or not to keep playing.
+        seeker (Seeker): The game's seeker.
+        terminal_service: For getting and displaying information on the terminal.
+    """
+
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self._word = Word()
+        self._is_playing = True
+        self._parachute = ParachuteMan()
+        self._terminal_service = TerminalService()
+        
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self._is_playing:
+            self._get_inputs()
+            self._do_updates()
+            self._do_outputs()
+
+    def _get_inputs(self):
+        """Moves the seeker to a new location.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        new_letter = self._word.get_letter()
+        self._seeker.move_location(new_location)
+        
+    def _do_updates(self):
+        """Keeps watch on where the seeker is moving.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        self._hider.watch_seeker(self._seeker)
+        
+    def _do_outputs(self):
+        """Provides a hint for the seeker to use.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        hint = self._hider.get_hint()
+        self._terminal_service.write_text(hint)
+        if self._hider.is_found():
+            self._is_playing = False
--- Jumper_game.py/parachute_display.py
+++ Jumper_game.py/parachute_display.py
@@ -1,1 +1,2 @@
 class ParachuteMan:
+    pass
